d5cdd5a4bb14b336b9d9112b3b60223541a3ccdb
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-07-22 13:18:54 +0900

    슬라이더 기능 업데이트

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: "/CssStudy8/", // REPO_NAME을 실제 리포지토리 이름으로 변경
-=======
-  base: "/CssStudy4/", // REPO_NAME을 실제 리포지토리 이름으로 변경
->>>>>>> a3920cd (재배포 2024.13:06)
+  base: "/CssStudy8/",
 });
